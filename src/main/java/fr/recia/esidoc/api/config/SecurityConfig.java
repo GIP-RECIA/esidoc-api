a75f891acfdfd4beb15436985580a6052bb46dba
cailbourdin <nathan.cailbourdin@recia.fr>
2025-09-18 16:50:14 +0200

    fix: session stateless car soffit

--- src/main/java/fr/recia/esidoc/api/config/SecurityConfig.java
+++ src/main/java/fr/recia/esidoc/api/config/SecurityConfig.java
@@ -24,6 +24,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
@@ -55,7 +56,7 @@ public class SecurityConfig {
                 .anyRequest().denyAll()
         );
 
-        http.sessionManagement(config -> config.sessionFixation().newSession());
+        http.sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
